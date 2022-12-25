c915868b8c7676d8be00eb7b3d9f9ca0209b5c32
PgmAdmin <prmorais1302@gmail.com>
2022-12-25 14:09:47 -0300

    Creating project, configuring Prettier, ESLint and Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'alurapic';
+
+  description = 'bike';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/6/66/Achol_Akhe_in_Bike.jpg';
 }
