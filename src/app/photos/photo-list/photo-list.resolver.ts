06cc7c5885b2823cd3eac7b52f591535c4995709
PgmAdmin <prmorais1302@gmail.com>
2022-12-26 16:40:54 -0300

    Adding Resolver to fetch photos

---
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoListResolver implements Resolve<Observable<Photo[]>> {
+  constructor(private photoService: PhotoService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Observable<Photo[]> {
+    const username = route.params['username'];
+    return this.photoService.listFromUser(username);
+  }
+}
