ccad0413887a4e91386847f2e69686369aebef15
PgmAdmin <prmorais1302@gmail.com>
2022-12-25 22:37:01 -0300

    Adding wildcard route for page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 import { PhotoModule } from './photos/photo.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PhotoModule],
+  imports: [BrowserModule, AppRoutingModule, PhotoModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
