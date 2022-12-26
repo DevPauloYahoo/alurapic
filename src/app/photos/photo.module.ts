e9128cfeac77ea9f166403524559fd46cf99b611
PgmAdmin <prmorais1302@gmail.com>
2022-12-26 16:25:43 -0300

    Filtering photos by description

--- src/app/photos/photo.module.ts
+++ src/app/photos/photo.module.ts
@@ -3,12 +3,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent],
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotosComponent,
+    FilterByDescriptionPipe,
+  ],
   imports: [CommonModule, HttpClientModule],
   exports: [PhotoListComponent],
 })
