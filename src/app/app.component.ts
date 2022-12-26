e6aeb1ba844d4ce3a55f9a35a8159d7e10a5f645
PgmAdmin <prmorais1302@gmail.com>
2022-12-25 22:02:34 -0300

    Adding AppRoutingModule and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Photo } from './photos/photo/photo';
-import { PhotoService } from './photos/photo/photo.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ap-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  photos: Photo[] = [];
-
-  constructor(private photoService: PhotoService) {}
-
-  ngOnInit(): void {
-    this.photoService.listFromUser('flavio').subscribe({
-      next: (photos) => (this.photos = photos),
-    });
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { PhotoModule } from './photos/photo.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, PhotoModule],
+  imports: [BrowserModule, AppRoutingModule, PhotoModule],
   providers: [],
   bootstrap: [AppComponent],
 })
