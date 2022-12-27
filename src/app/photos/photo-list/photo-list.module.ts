167c58d24763c525404fa9198d421837ba371422
PgmAdmin <prmorais1302@gmail.com>
2022-12-27 14:39:35 -0300

    Adding custom directive for mouse hover event

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CardModule } from '../../shared/components/card/card.module';
+import { DarkenOnHoverModule } from '../../shared/directives/darken-on-hover/darken-on-hover.module';
 import { PhotoModule } from '../photo/photo.module';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
@@ -17,6 +18,6 @@ import { SearchComponent } from './search/search.component';
     FilterByDescriptionPipe,
     SearchComponent,
   ],
-  imports: [CommonModule, PhotoModule, CardModule],
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule],
 })
 export class PhotoListModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { DarkenOnHoverModule } from './directives/darken-on-hover/darken-on-hover.module';
+
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, DarkenOnHoverModule],
 })
 export class SharedModule {}
