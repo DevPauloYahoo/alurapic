3414c313e7efaf3825c720d98dda8c0a2242bc3f
PgmAdmin <prmorais1302@gmail.com>
2022-12-26 22:20:36 -0300

    Adding more feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
-import { PhotoModule } from './photos/photo.module';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PhotoModule, ErrorsModule],
+  imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
