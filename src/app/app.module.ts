d5db1df67a346b238c597b651025e8847d9673e8
PgmAdmin <prmorais1302@gmail.com>
2022-12-29 15:30:26 -0300

    Adding Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { CoreModule } from './core/core.module';
 import { ErrorsModule } from './errors/errors.module';
 import { HomeModule } from './home/home.module';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule, HomeModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PhotosModule,
+    ErrorsModule,
+    HomeModule,
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
