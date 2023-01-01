55c1a01de68cc247e6d02a537fddc59d413d2b0b
PgmAdmin <prmorais1302@gmail.com>
2023-01-01 18:34:05 -0300

    Starting registration form validation

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,8 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+
+import { FormValidationService } from '../../shared/form-validation.service';
 
 @Component({
   selector: 'ap-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private formValidationService: FormValidationService,
+  ) {}
+
+  get email() {
+    return this.signupForm?.get('email');
+  }
+
+  get fullName() {
+    return this.signupForm?.get('fullName');
+  }
+
+  get userName() {
+    return this.signupForm?.get('userName');
+  }
+
+  get password() {
+    return this.signupForm?.get('password');
+  }
+
+  ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: [
+        '',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(50)],
+      ],
+      userName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(30),
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+        ],
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(15)],
+      ],
+    });
+  }
+
+  validationFields(fieldName: string) {
+    return this.formValidationService.errorMessage(fieldName, this.signupForm);
+  }
+}
--- src/app/shared/form-validation.service.ts
+++ src/app/shared/form-validation.service.ts
@@ -19,6 +19,21 @@ export class FormValidationService {
       return `${fieldName} dever ter no mínimo ${requiredLength} caracteres`;
     }
 
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field?.errors
+        ? field.errors['maxlength']['requiredLength']
+        : 50;
+      return `${fieldName} dever ter no máximo ${requiredLength} caracteres`;
+    }
+
+    if (field?.hasError('email')) {
+      return `${fieldName} inválido`;
+    }
+
+    if (field?.hasError('pattern')) {
+      return `${fieldName} deve ser em letras minúsculas e não pode começar com números`;
+    }
+
     return 'campo inválido';
   }
 }
