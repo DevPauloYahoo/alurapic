5be4d12f12720a1554a2ea69d84262be4b900498
PgmAdmin <prmorais1302@gmail.com>
2023-01-01 16:06:21 -0300

    Implementing Logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { User } from '../user/user';
@@ -14,10 +15,15 @@ export class HeaderComponent {
 
   // user: User | undefined;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
     this.user$ = userService.getUser();
     // this.user$.subscribe({
     //   next: (user) => (this.user = user),
     // });
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
   }
 }
