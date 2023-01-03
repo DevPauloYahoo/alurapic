3b887fe1400fe68a1b0dd8d0272b753aea69f461
Paulo Roberto De Morais <prmorais1302@gmail.com>
2023-01-03 10:40:22 -0300

    Adding footer in application

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { RequestInterceptor } from './auth/request.interceptor';
+import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
