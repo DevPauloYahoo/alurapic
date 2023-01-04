bfce1e6fc1c8d709a5a7f1ccdde1d30659c7ab65
PgmAdmin <prmorais1302@gmail.com>
2023-01-03 23:01:09 -0300

    Constructing Photos Details component

---
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo-details',
+  templateUrl: './photo-details.component.html',
+  styleUrls: ['./photo-details.component.scss'],
+})
+export class PhotoDetailsComponent {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { PhotoDetailsModule } from './photo-details/photo-details.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
 import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
   declarations: [],
-  imports: [PhotoModule, PhotoFormModule, PhotoListModule],
+  imports: [PhotoModule, PhotoFormModule, PhotoListModule, PhotoDetailsModule],
   exports: [],
 })
 export class PhotosModule {}
