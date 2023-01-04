7f0db5d997ea92e07c6ba40a2af2af60fa46b4f2
PgmAdmin <prmorais1302@gmail.com>
2023-01-04 19:47:44 -0300

    Adding validation to the comment form

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
+import { FormValidationService } from '../../../shared/form-validation.service';
 import { PhotoComment } from '../../photo/photo-comment.interface';
 import { PhotoService } from '../../photo/photo.service';
 
@@ -12,10 +14,30 @@ import { PhotoService } from '../../photo/photo.service';
 export class PhotoCommentsComponent implements OnInit {
   comments$: Observable<PhotoComment[]> = new Observable<PhotoComment[]>();
   @Input() photoId: number | undefined;
+  commentForm!: FormGroup;
 
-  constructor(private photoService: PhotoService) {}
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private photoService: PhotoService,
+    private formValidationService: FormValidationService,
+  ) {}
+
+  get comment() {
+    return this.commentForm.get('comment');
+  }
 
   ngOnInit(): void {
     this.comments$ = this.photoService.getComments(this.photoId!);
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.maxLength(300)],
+    });
+  }
+
+  validationFields(fieldName: string, translatedField: string) {
+    return this.formValidationService.errorMessage(
+      fieldName,
+      translatedField,
+      this.commentForm,
+    );
   }
 }
--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { PhotoModule } from '../photo/photo.module';
@@ -8,6 +9,6 @@ import { PhotoDetailsComponent } from './photo-details.component';
 
 @NgModule({
   declarations: [PhotoDetailsComponent, PhotoCommentsComponent],
-  imports: [CommonModule, RouterModule, PhotoModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, PhotoModule],
 })
 export class PhotoDetailsModule {}
