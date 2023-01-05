29dffd7697456b365450b67256e5cad9030d5c58
Paulo Roberto De Morais <prmorais1302@gmail.com>
2023-01-05 14:49:09 -0300

    Implementing alert service

--- src/app/shared/components/alert-global/alert-global.service.ts
+++ src/app/shared/components/alert-global/alert-global.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Alert, AlertType } from './alert.class';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AlertGlobalService {
-  constructor() {}
+  alertSubject: Subject<Alert> = new Subject<Alert>();
+
+  success(message: string): void {
+    this.alert(AlertType.SUCCESS, message);
+  }
+
+  info(message: string): void {
+    this.alert(AlertType.INFO, message);
+  }
+
+  warning(message: string): void {
+    this.alert(AlertType.WARNING, message);
+  }
+
+  danger(message: string): void {
+    this.alert(AlertType.DANGER, message);
+  }
+
+  getAlert() {
+    return this.alertSubject.asObservable();
+  }
+
+  private alert(alertType: AlertType, message: string): void {
+    this.alertSubject.next(new Alert(alertType, message));
+  }
 }
